ebc810904e06fc356c37931b68e7f8e05d8e1ca7
Nikita Shubin <nikita.shubin@maquefel.me>
2021-03-04 17:55:14 +0300

    clocksource: ep93xx: fixme

--- drivers/clocksource/timer-ep93xx.c
+++ drivers/clocksource/timer-ep93xx.c
@@ -13,7 +13,11 @@
 #include <linux/sched_clock.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/of_irq.h>
+#include <linux/of_address.h>
 #include <linux/io.h>
+#include <linux/regmap.h>
+#include <linux/mfd/syscon.h>
 #include <asm/mach/time.h>
 
 /*************************************************************************
@@ -35,77 +39,78 @@
  *************************************************************************
  */
 
-#define IRQ_EP93XX_VIC0			1
-#define IRQ_EP93XX_VIC1			(IRQ_EP93XX_VIC0 + 32)
-#define IRQ_EP93XX_TIMER3		(IRQ_EP93XX_VIC1 + 19)
-#define EP93XX_APB_VIRT_BASE            0xfed00000
-#define EP93XX_APB_IOMEM(x)             IOMEM(EP93XX_APB_VIRT_BASE + (x))
-#define EP93XX_TIMER_BASE		EP93XX_APB_IOMEM(0x00010000)
-#define EP93XX_TIMER_REG(x)		(EP93XX_TIMER_BASE + (x))
-
-
-#define EP93XX_TIMER1_LOAD		EP93XX_TIMER_REG(0x00)
-#define EP93XX_TIMER1_VALUE		EP93XX_TIMER_REG(0x04)
-#define EP93XX_TIMER1_CONTROL		EP93XX_TIMER_REG(0x08)
-#define EP93XX_TIMER123_CONTROL_ENABLE	(1 << 7)
-#define EP93XX_TIMER123_CONTROL_MODE	(1 << 6)
-#define EP93XX_TIMER123_CONTROL_CLKSEL	(1 << 3)
-#define EP93XX_TIMER1_CLEAR		EP93XX_TIMER_REG(0x0c)
-#define EP93XX_TIMER2_LOAD		EP93XX_TIMER_REG(0x20)
-#define EP93XX_TIMER2_VALUE		EP93XX_TIMER_REG(0x24)
-#define EP93XX_TIMER2_CONTROL		EP93XX_TIMER_REG(0x28)
-#define EP93XX_TIMER2_CLEAR		EP93XX_TIMER_REG(0x2c)
-#define EP93XX_TIMER4_VALUE_LOW		EP93XX_TIMER_REG(0x60)
-#define EP93XX_TIMER4_VALUE_HIGH	EP93XX_TIMER_REG(0x64)
-#define EP93XX_TIMER4_VALUE_HIGH_ENABLE	(1 << 8)
-#define EP93XX_TIMER3_LOAD		EP93XX_TIMER_REG(0x80)
-#define EP93XX_TIMER3_VALUE		EP93XX_TIMER_REG(0x84)
-#define EP93XX_TIMER3_CONTROL		EP93XX_TIMER_REG(0x88)
-#define EP93XX_TIMER3_CLEAR		EP93XX_TIMER_REG(0x8c)
+#define EP93XX_TIMER1_LOAD		0x00
+#define EP93XX_TIMER1_VALUE		0x04
+#define EP93XX_TIMER1_CONTROL		0x08
+#define EP93XX_TIMER123_CONTROL_ENABLE	BIT(7)
+#define EP93XX_TIMER123_CONTROL_MODE	BIT(6)
+#define EP93XX_TIMER123_CONTROL_CLKSEL	BIT(3)
+#define EP93XX_TIMER1_CLEAR		0x0c
+#define EP93XX_TIMER2_LOAD		0x20
+#define EP93XX_TIMER2_VALUE		0x24
+#define EP93XX_TIMER2_CONTROL		0x28
+#define EP93XX_TIMER2_CLEAR		0x2c
+#define EP93XX_TIMER4_VALUE_LOW		0x60
+#define EP93XX_TIMER4_VALUE_HIGH	0x64
+#define EP93XX_TIMER4_VALUE_HIGH_ENABLE	BIT(8)
+#define EP93XX_TIMER3_LOAD		0x80
+#define EP93XX_TIMER3_VALUE		0x84
+#define EP93XX_TIMER3_CONTROL		0x88
+#define EP93XX_TIMER3_CLEAR		0x8c
 
 #define EP93XX_TIMER123_RATE		508469
 #define EP93XX_TIMER4_RATE		983040
 
+struct ep93xx_tcu {
+	void __iomem *base;
+};
+
+static struct ep93xx_tcu *ep93xx_tcu;
+
 static u64 notrace ep93xx_read_sched_clock(void)
 {
+	struct ep93xx_tcu *tcu = ep93xx_tcu;
 	u64 ret;
-	
-	ret = readl(EP93XX_TIMER4_VALUE_LOW);
-	ret |= ((u64) (readl(EP93XX_TIMER4_VALUE_HIGH) & 0xff) << 32);
+
+	ret = readl(tcu->base + EP93XX_TIMER4_VALUE_LOW);
+	ret |= ((u64) (readl(tcu->base + EP93XX_TIMER4_VALUE_HIGH) & 0xff) << 32);
 	return ret;
 }
 
 static u64 ep93xx_clocksource_read(struct clocksource *c)
 {
+	struct ep93xx_tcu *tcu = ep93xx_tcu;
 	u64 ret;
 	
-	ret = readl(EP93XX_TIMER4_VALUE_LOW);
-	ret |= ((u64) (readl(EP93XX_TIMER4_VALUE_HIGH) & 0xff) << 32);
+	ret = readl(tcu->base + EP93XX_TIMER4_VALUE_LOW);
+	ret |= ((u64) (readl(tcu->base + EP93XX_TIMER4_VALUE_HIGH) & 0xff) << 32);
 	return (u64) ret;
 }
 
 static int ep93xx_clkevt_set_next_event(unsigned long next,
 					struct clock_event_device *evt)
 {
+	struct ep93xx_tcu *tcu = ep93xx_tcu;
 	/* Default mode: periodic, off, 508 kHz */
 	u32 tmode = EP93XX_TIMER123_CONTROL_MODE |
 	EP93XX_TIMER123_CONTROL_CLKSEL;
 	
 	/* Clear timer */
-	writel(tmode, EP93XX_TIMER3_CONTROL);
+	writel(tmode, tcu->base + EP93XX_TIMER3_CONTROL);
 	
 	/* Set next event */
-	writel(next, EP93XX_TIMER3_LOAD);
+	writel(next, tcu->base + EP93XX_TIMER3_LOAD);
 	writel(tmode | EP93XX_TIMER123_CONTROL_ENABLE,
-	       EP93XX_TIMER3_CONTROL);
+	       tcu->base + EP93XX_TIMER3_CONTROL);
 	return 0;
 }
 
 
 static int ep93xx_clkevt_shutdown(struct clock_event_device *evt)
 {
+	struct ep93xx_tcu *tcu = ep93xx_tcu;
 	/* Disable timer */
-	writel(0, EP93XX_TIMER3_CONTROL);
+	writel(0, tcu->base + EP93XX_TIMER3_CONTROL);
 	
 	return 0;
 }
@@ -122,10 +127,11 @@ static struct clock_event_device ep93xx_clockevent = {
 
 static irqreturn_t ep93xx_timer_interrupt(int irq, void *dev_id)
 {
+	struct ep93xx_tcu *tcu = ep93xx_tcu;
 	struct clock_event_device *evt = dev_id;
 	
 	/* Writing any value clears the timer interrupt */
-	writel(1, EP93XX_TIMER3_CLEAR);
+	writel(1, tcu->base + EP93XX_TIMER3_CLEAR);
 	
 	evt->event_handler(evt);
 	
@@ -134,15 +140,38 @@ static irqreturn_t ep93xx_timer_interrupt(int irq, void *dev_id)
 
 static int __init ep93xx_timer_of_init(struct device_node *np)
 {
-	int irq = IRQ_EP93XX_TIMER3;
+	int irq;
 	unsigned long flags = IRQF_TIMER | IRQF_IRQPOLL;
+	struct ep93xx_tcu *tcu;
+	int ret;
+
+	tcu = kzalloc(sizeof(*tcu), GFP_KERNEL);
+	if (!tcu)
+		return -ENOMEM;
 	
+	tcu->base = of_iomap(np, 0);
+	if (!tcu->base) {
+		pr_err("Can't remap registers\n");
+		ret = -ENXIO;
+		goto out_free;
+	}
+
+	ep93xx_tcu = tcu;	
+	pr_info("%s : base=0x%px\n", __func__, tcu->base);
+	
+	irq = irq_of_parse_and_map(np, 1);
+	if (irq <= 0) {
+		pr_err("ERROR: invalid interrupt number\n");
+		ret = -EINVAL;
+		goto out_free;
+	}
+
 	/* Enable and register clocksource and sched_clock on timer 4 */
 	writel(EP93XX_TIMER4_VALUE_HIGH_ENABLE,
-	       EP93XX_TIMER4_VALUE_HIGH);
+	       tcu->base + EP93XX_TIMER4_VALUE_HIGH);
 	clocksource_mmio_init(NULL, "timer4",
-			      EP93XX_TIMER4_RATE, 200, 40,
-		       ep93xx_clocksource_read);
+				EP93XX_TIMER4_RATE, 200, 40,
+				ep93xx_clocksource_read);
 	sched_clock_register(ep93xx_read_sched_clock, 40,
 			     EP93XX_TIMER4_RATE);
 	
@@ -151,11 +180,15 @@ static int __init ep93xx_timer_of_init(struct device_node *np)
 		&ep93xx_clockevent))
 		pr_err("Failed to request irq %d (ep93xx timer)\n", irq);
 	clockevents_config_and_register(&ep93xx_clockevent,
-					EP93XX_TIMER123_RATE,
-				 1,
-				 0xffffffffU);
+				EP93XX_TIMER123_RATE,
+				1,
+				0xffffffffU);
 
 	return 0;
+	
+	out_free:
+	kfree(tcu);
+	return ret;
 }
 
 TIMER_OF_DECLARE(ep93xx_timer, "cirrus,ep93xx-timer", ep93xx_timer_of_init);
